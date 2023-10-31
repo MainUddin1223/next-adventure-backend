765453f0ff7e1e9ac74c7505df1fc5a9f6fa618c
Md Main Uddin <2021md.main@gmail.com>
2023-10-31 13:45:09 +0600

    husky testing

--- src/utils/helpers/pagination.ts
+++ src/utils/helpers/pagination.ts
@@ -1,20 +1,20 @@
 import { IPaginationPayload, IPaginationValue } from './interface';
-
+console.log(error)
 export const pagination = (data: IPaginationPayload): IPaginationValue => {
-  const {
-    page = 1,
-    limit = 10,
-    sortBy = 'createdAt',
-    sortOrder = 'desc',
-  } = data;
-  const sortOption = {
-    [sortBy]: sortOrder,
-  };
-  const options = {
-    ['skip']: Number(page) == 0 ? 0 : (Number(page) - 1) * Number(limit),
-    ['take']: Number(limit),
-    ['orderBy']: sortOption,
-    ['page']: Number(page),
-  };
-  return options;
+    const {
+        page = 1,
+        limit = 10,
+        sortBy = 'createdAt',
+        sortOrder = 'desc',
+    } = data;
+    const sortOption = {
+        [sortBy]: sortOrder,
+    };
+    const options = {
+        ['skip']: Number(page) == 0 ? 0 : (Number(page) - 1) * Number(limit),
+        ['take']: Number(limit),
+        ['orderBy']: sortOption,
+        ['page']: Number(page),
+    };
+    return options;
 };
