ce8e0196fae30f1d529c449870d05a04fd39dc9c
Md Main Uddin <2021md.main@gmail.com>
2023-11-05 11:49:34 +0600

    validation added on signup and register api

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -3,25 +3,49 @@ import catchAsync from '../../utils/errorHandlers/catchAsync';
 import { authService } from './auth.service';
 import sendResponse from '../../utils/helpers/sendResponse';
 import { StatusCodes } from 'http-status-codes';
+import { agencyRegisterSchema, signUpSchema } from './auth.validation';
 
 const signUp = catchAsync(async (req: Request, res: Response) => {
-  const result = await authService.signUp(req.body);
-  sendResponse(res, {
-    statusCode: StatusCodes.OK,
-    success: true,
-    message: 'Signup successful',
-    data: result,
-  });
+  const { error } = await signUpSchema.validate(req.body);
+
+  if (error) {
+    sendResponse(res, {
+      statusCode: StatusCodes.NON_AUTHORITATIVE_INFORMATION,
+      success: false,
+      message: error.details[0]?.message || 'Sign up failed',
+      data: error.details,
+    });
+  } else {
+    const result = await authService.signUp(req.body);
+    sendResponse(res, {
+      statusCode: StatusCodes.OK,
+      success: true,
+      message: 'Signup successful',
+      data: result,
+    });
+  }
 });
 
 const registerAgency = catchAsync(async (req: Request, res: Response) => {
-  const result = await authService.registerAgency(req.body);
-  sendResponse(res, {
-    statusCode: StatusCodes.OK,
-    success: true,
-    message: 'Agency registered successfully',
-    data: result,
-  });
+  const { error } = await agencyRegisterSchema.validate(req.body);
+
+  if (error) {
+    console.log(error.details);
+    sendResponse(res, {
+      statusCode: StatusCodes.NON_AUTHORITATIVE_INFORMATION,
+      success: false,
+      message: error.details[0]?.message || 'Sign up failed',
+      data: error.details,
+    });
+  } else {
+    const result = await authService.registerAgency(req.body);
+    sendResponse(res, {
+      statusCode: StatusCodes.OK,
+      success: true,
+      message: 'Agency registered successfully',
+      data: result,
+    });
+  }
 });
 
 export const authController = { signUp, registerAgency };
