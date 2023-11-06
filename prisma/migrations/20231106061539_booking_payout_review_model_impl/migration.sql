38fed63ffe218ef50df12553f91aa0d3fd1e842f
Md Main Uddin <2021md.main@gmail.com>
2023-11-06 12:18:36 +0600

    database migration

---
+++ prisma/migrations/20231106061539_booking_payout_review_model_impl/migration.sql
@@ -0,0 +1,132 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Plan` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Reviews` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- CreateEnum
+CREATE TYPE "PayoutStatus" AS ENUM ('pending', 'released', 'postponded');
+
+-- CreateEnum
+CREATE TYPE "BookingStatus" AS ENUM ('canceled', 'confirmed', 'pending', 'rejected');
+
+-- DropForeignKey
+ALTER TABLE "Plan" DROP CONSTRAINT "Plan_agencyId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Reviews" DROP CONSTRAINT "Reviews_userId_fkey";
+
+-- DropTable
+DROP TABLE "Plan";
+
+-- DropTable
+DROP TABLE "Reviews";
+
+-- CreateTable
+CREATE TABLE "plan" (
+    "id" SERIAL NOT NULL,
+    "agencyId" INTEGER NOT NULL,
+    "planName" TEXT NOT NULL,
+    "destination" TEXT NOT NULL,
+    "departureFrom" TEXT NOT NULL,
+    "coverLocations" TEXT[],
+    "price" DECIMAL(10,2) NOT NULL,
+    "images" TEXT[],
+    "duration" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "departureTime" TIMESTAMP(3) NOT NULL,
+    "meals" TEXT NOT NULL,
+    "events" TEXT[],
+    "notAllowed" TEXT[],
+    "deadline" TIMESTAMP(3) NOT NULL,
+    "featured" BOOLEAN NOT NULL DEFAULT false,
+    "status" "PlanStatus" NOT NULL DEFAULT 'available',
+    "totalSeats" INTEGER NOT NULL,
+    "totalBooking" INTEGER NOT NULL DEFAULT 0,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "plan_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "bookings" (
+    "id" SERIAL NOT NULL,
+    "status" "BookingStatus" NOT NULL DEFAULT 'pending',
+    "seats" INTEGER NOT NULL,
+    "totalAmount" DECIMAL(10,2) NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "agencyId" INTEGER NOT NULL,
+    "planId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "bookings_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "reviews" (
+    "id" SERIAL NOT NULL,
+    "rating" INTEGER NOT NULL,
+    "feedback" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "reviews_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "planReviews" (
+    "id" SERIAL NOT NULL,
+    "rating" INTEGER NOT NULL,
+    "feedback" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "agencyId" INTEGER NOT NULL,
+    "planId" INTEGER NOT NULL,
+
+    CONSTRAINT "planReviews_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "payouts" (
+    "id" SERIAL NOT NULL,
+    "status" "PayoutStatus" NOT NULL DEFAULT 'pending',
+    "agencyId" INTEGER NOT NULL,
+    "planId" INTEGER NOT NULL,
+    "bookingId" INTEGER NOT NULL,
+
+    CONSTRAINT "payouts_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "plan" ADD CONSTRAINT "plan_agencyId_fkey" FOREIGN KEY ("agencyId") REFERENCES "agencies"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "bookings" ADD CONSTRAINT "bookings_agencyId_fkey" FOREIGN KEY ("agencyId") REFERENCES "agencies"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "bookings" ADD CONSTRAINT "bookings_planId_fkey" FOREIGN KEY ("planId") REFERENCES "plan"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "bookings" ADD CONSTRAINT "bookings_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "reviews" ADD CONSTRAINT "reviews_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "planReviews" ADD CONSTRAINT "planReviews_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "planReviews" ADD CONSTRAINT "planReviews_agencyId_fkey" FOREIGN KEY ("agencyId") REFERENCES "agencies"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "planReviews" ADD CONSTRAINT "planReviews_planId_fkey" FOREIGN KEY ("planId") REFERENCES "plan"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "payouts" ADD CONSTRAINT "payouts_agencyId_fkey" FOREIGN KEY ("agencyId") REFERENCES "agencies"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "payouts" ADD CONSTRAINT "payouts_planId_fkey" FOREIGN KEY ("planId") REFERENCES "plan"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "payouts" ADD CONSTRAINT "payouts_bookingId_fkey" FOREIGN KEY ("bookingId") REFERENCES "bookings"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
